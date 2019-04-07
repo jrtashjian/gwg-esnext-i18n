b6a6e726b8e0624d777f84e33d0f123079aa95a9
JR Tashjian <jrtashjian@gmail.com>
2019-04-07 18:31:31 -0400

    Internationalize our strings.

--- block.js
+++ block.js
@@ -1,14 +1,15 @@
+const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
 
 registerBlockType('gwg/esnext-starter', {
-    title: 'Get With Gutenberg - ESNext Starter',
+    title: __('Get With Gutenberg - ESNext Starter', 'gwg'),
     category: 'common',
 
     edit(props) {
-        return <p className={props.className}>Hello editor.</p>;
+        return <p className={props.className}>{__('Hello editor.', 'gwg')}</p>;
     },
 
     save(props) {
-        return <p className={props.className}>Hello saved content.</p>;
+        return <p className={props.className}>{__('Hello saved content.', 'gwg')}</p>;
     },
 });
